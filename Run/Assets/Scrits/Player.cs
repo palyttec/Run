256305d308f15aadf1fccd8f14e0c51d833db83b
sarana <sarana.rb.ru@gmail.com>
2020-10-26 03:29:28 +0700

    Добавление дорожки, героя и его движение

---
+++ Run/Assets/Scrits/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocity = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Изменения вызывающиеся за один кадр
+    void Update()
+    { // передвижение героя
+        transform.position += new Vector3(velocity * Time.deltaTime, 0, 0);
+    }
+}
